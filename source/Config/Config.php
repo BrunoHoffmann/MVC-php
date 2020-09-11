1d48c61cda421c56a37e8e03848195aff4fe259d
Bruno Hoffmann <brunooficial547@hotmail.com>
2020-09-11 17:00:31 -0300

    alter db name

--- source/Config/Config.php
+++ source/Config/Config.php
@@ -5,6 +5,6 @@
  */
 define('CONF_DB_DRIVER', 'mysql');
 define('CONF_DB_HOST', 'localhost');
-define('CONF_DB_NAME', 'blog');
+define('CONF_DB_NAME', 'db');
 define('CONF_DB_USER', 'root');
 define('CONF_DB_PASS', '');
